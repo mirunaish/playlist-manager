8e3370c781c40ab24319bcb8a6832dad4f63d8ae
miruna <miruna.palaghean@gmail.com>
2023-12-08 04:27:30 -0700

    added placeholder image

--- extension/src/views/popup/components/Thumbnail.js
+++ extension/src/views/popup/components/Thumbnail.js
@@ -1,5 +1,7 @@
 import React, { useCallback, useMemo, useState } from "react";
 
+const placeholderPath = "../../placeholder.png";
+
 function Thumbnail({ src, maxWidth = 200, square = false }) {
   const [width, height] = useMemo(() => {
     return [maxWidth, square ? maxWidth : (maxWidth * 3) / 4];
@@ -7,10 +9,14 @@ function Thumbnail({ src, maxWidth = 200, square = false }) {
 
   return (
     <img
-      src={src}
+      // className="hue"
+      src={src ?? placeholderPath}
       width={width.toString() + "px"}
       height={height.toString() + "px"}
       alt={"thumbnail"}
+      onError={(e) => {
+        e.target.src = placeholderPath;
+      }} // if the image cannot be loaded, use placeholder
     ></img>
   );
 }
