049cd3b58f161d3c20282baf1d77b412f5691c81
miruna <miruna.palaghean@gmail.com>
2023-12-11 00:26:04 -0700

    improved icons

--- extension/src/views/popup/icons/index.js
+++ extension/src/views/popup/icons/index.js
@@ -1,10 +1,4 @@
-import { ReactComponent as Left } from "./left.svg";
-import { ReactComponent as Right } from "./right.svg";
-import { ReactComponent as Star4 } from "./star4.svg";
-import { ReactComponent as Star5 } from "./star5.svg";
-import { ReactComponent as Star6 } from "./star6.svg";
-import { ReactComponent as Youtube } from "./youtube_logo.svg";
-import { ReactComponent as Soundcloud } from "./soundcloud_logo.svg";
+import { useEffect, useState } from "react";
 
 export const Icons = {
   FILL: "fill",
@@ -24,33 +18,27 @@ export const Icons = {
 
   YOUTUBE: "youtube",
   SOUNDCLOUD: "soundcloud",
+  SPOTIFY: "youtube", // TODO add icon
 };
 
 export function Icon({ icon, size = 15, color = null, type = "", ...args }) {
+  const [Svg, setSvg] = useState(null);
+
+  // get icon from file
+  useEffect(() => {
+    (async () => {
+      const { ReactComponent } = await import("./" + icon + ".svg");
+      setSvg(ReactComponent);
+    })();
+  }, [icon]);
+
+  if (Svg == null) return <p>loading icon</p>;
+
   const props = {
     ...args,
     className: (args.className ?? "") + " " + type,
     width: size,
     height: size,
   };
-
-  // TODO
-  switch (icon) {
-    case Icons.LEFT:
-      return <Left {...props} />;
-    case Icons.RIGHT:
-      return <Right {...props} />;
-    case Icons.STAR4:
-      return <Star4 {...props} />;
-    case Icons.STAR5:
-      return <Star5 {...props} />;
-    case Icons.STAR6:
-      return <Star6 {...props} />;
-    case Icons.YOUTUBE:
-      return <Youtube {...props} />;
-    case Icons.SOUNDCLOUD:
-      return <Soundcloud {...props} />;
-    default:
-      return null;
-  }
+  return <Svg {...props} />;
 }
