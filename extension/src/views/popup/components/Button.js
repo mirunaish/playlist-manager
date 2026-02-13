d52b30896ab9eacecff4c14c4bd8d9e5802ccfbe
miruna <58366207+mirunaish@users.noreply.github.com>
2026-02-12 23:19:22 -0500

    fixed button.

--- extension/src/views/popup/components/Button.js
+++ extension/src/views/popup/components/Button.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Icon } from "../icons";
 
 function Button({
@@ -6,16 +6,29 @@ function Button({
   icon = null, // should be an object with icon props
   primary = false,
   onClick = () => {},
+  className = "",
+  style = {},
 }) {
+  const iconDetails = useMemo(() => {
+    if (!icon) return {};
+    if (typeof icon === "object") return icon;
+    return { icon };
+  }, [icon]);
+
   return (
     <button
-      className={(primary ? "primary " : "") + (title.length > 0 ? "wide" : "")}
+      className={
+        (primary ? "primary " : "") +
+        (title.length > 0 ? "wide " : "") +
+        className
+      }
       onClick={onClick}
+      style={style}
     >
       {icon && (
         // if given both a title and an icon, put padding between them
         <Icon
-          {...icon}
+          {...iconDetails}
           className={icon.className + " disabled"}
           style={{ ...icon.style, ...(title ? { marginRight: 5 } : {}) }}
         />
